df219f43df1de3a9ed504cf7da095bec6fb56793
Alex B Carr <mralex.carr@yahoo.com>
2023-03-20 18:50:54 -0700

    initial City

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constrctor(props) {
+    super(props);
+    this.setState = {
+      city: '',
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+  }
+
+  getCityData = async (event) => {
+    event.preventDefault();
+
+    try {
+     
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.pk.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+
+      let cityDataFromAxios = await axios.get(url);
+
+      console.log(cityDataFromAxios.data[0])
+
+      
+      this.setState({
+        cityData: cityDataFromAxios.data[0],
+        error: false
+      });
+
+    } catch (error) {
+
+      
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }
+
+  }
+
+  
+
+  render() {
+    return (
+      <>
+        <h1>API CALLS</h1>
+
+        <Form onSubmit={this.getCityData}>
+          <label > Enter in a City:
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
+          <Button type="submit">Explore!</Button>
+        </Form>
+
+
+        
+          this.state.error
+            ? <p>{this.state.errorMessage}</p>
+            : <p>{this.state.cityData.display_name}</p>
+            <p>{this.state.cityData.lat}</p>
+            <p>{this.state.cityData.lon}</p>
+        
+      </>
+    ) 
+  }
 }
+
+
+
+
+
+
+
+
+
+
 
 export default App;
